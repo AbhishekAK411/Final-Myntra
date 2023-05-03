4c55345d7efe1426361fadaa23a682eaefdad56d
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-05-03 18:35:41 +0530

    logout functionality added

--- onload.js
+++ onload.js
@@ -10,7 +10,17 @@ window.onload = () =>{
         var h5 = document.createElement("h5");
         h5.innerText = myntra_currentUser.userName;
         myntra_div.append(h5);
+
+        var logout_div = document.getElementById("myntra_logout");
+        var logout = `<button class="logout_style" onclick="myntra_logout()"><i class="fa-solid fa-arrow-right-from-bracket"></i> <h5>logout</h5></button>`;
+        logout_div.innerHTML = logout;
+
     } else {
         alert("Current User is absent");
     }
+}
+
+function myntra_logout(){
+    localStorage.removeItem("myntra_current_user");
+    window.location.reload();
 }
