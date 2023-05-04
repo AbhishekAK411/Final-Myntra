30a89eac3dd98d802a062c23ed14b4e6a13e2f3a
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-05-04 19:15:37 +0530

    update onload.js

--- onload.js
+++ onload.js
@@ -2,16 +2,6 @@ window.onload = () =>{
     var myntra_currentUser = JSON.parse(localStorage.getItem("myntra_current_user"));
     // console.log(myntra_currentUser);
 
-    var ls = JSON.parse(localStorage.getItem("myntra_prodData"));
-    var myntra_data = "";
-    var divToInsertProducts = document.getElementById("myntra_insert");
-
-    for(var i=0;i<ls.length;i++){
-        myntra_data += `<div><img src="${ls[i].m_prodImg}" alt="" /> <h4>${ls[i].m_prodName}</h4> <p>${ls[i].m_prodDesc}</p> <h5>Rs. ${ls[i].m_prodPrice} <s>Rs.1299</s> <b>(66% off)</b></h5></div>`;
-        divToInsertProducts.innerHTML = myntra_data;
-    }
-
-
     if(myntra_currentUser){
         var myntra_div = document.getElementById("myntra_current_user");
         var removeTag = document.getElementById("myntra_remove_tag");
@@ -27,6 +17,15 @@ window.onload = () =>{
 
     } else {
         console.log("Current User is absent");
+    }
+
+    var ls = JSON.parse(localStorage.getItem("myntra_prodData"));
+    var myntra_data = "";
+    var divToInsertProducts = document.getElementById("myntra_insert");
+
+    for(var i=0;i<ls.length;i++){
+        myntra_data += `<div><img src="${ls[i].m_prodImg}" alt="" /> <h4>${ls[i].m_prodName}</h4> <p>${ls[i].m_prodDesc}</p> <h5>Rs. ${ls[i].m_prodPrice} <s>Rs.1299</s> <b>(66% off)</b></h5></div>`;
+        divToInsertProducts.innerHTML = myntra_data;
     }
 }
 
