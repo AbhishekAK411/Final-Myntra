fc3ceec0eb2dd3c93f1a583a0bdeee6587ad0884
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-05-05 19:13:59 +0530

    added single product functionality

--- onload.js
+++ onload.js
@@ -24,9 +24,17 @@ window.onload = () =>{
     var divToInsertProducts = document.getElementById("myntra_insert");
 
     for(var i=0;i<ls.length;i++){
-        myntra_data += `<div><img src="${ls[i].m_prodImg}" alt="" /> <h4>${ls[i].m_prodName}</h4> <p>${ls[i].m_prodDesc}</p> <h5>Rs. ${ls[i].m_prodPrice} <s>Rs.1299</s> <b>(66% off)</b></h5></div>`;
+        myntra_data += `<div onclick='myntra_redirect(${JSON.stringify(ls[i])})'><img src="${ls[i].m_prodImg}" alt="" /> <h4>${ls[i].m_prodName}</h4> <p>${ls[i].m_prodDesc}</p> <h5>Rs. ${ls[i].m_prodPrice} <s>Rs.1299</s> <b>(66% off)</b></h5></div>`;
         divToInsertProducts.innerHTML = myntra_data;
     }
+}
+
+function myntra_redirect(myntra_product){
+    var myntra_currentProduct = JSON.stringify(myntra_product);
+
+    localStorage.setItem("myntra_currentProd", myntra_currentProduct);
+    window.location.href = `./singleproduct.html`;
+
 }
 
 function myntra_logout(){
