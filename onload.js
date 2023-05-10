766e321d9502a7c236cc13c5a2f270203ef4a0c8
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-05-10 08:07:18 +0530

    commit

--- onload.js
+++ onload.js
@@ -2,22 +2,6 @@ window.onload = () =>{
     var myntra_currentUser = JSON.parse(localStorage.getItem("myntra_current_user"));
     // console.log(myntra_currentUser);
 
-    if(myntra_currentUser){
-        var myntra_div = document.getElementById("myntra_current_user");
-        var removeTag = document.getElementById("myntra_remove_tag");
-        removeTag.remove();
-
-        var h5 = document.createElement("h5");
-        h5.innerText = myntra_currentUser.userName;
-        myntra_div.append(h5);
-
-        var logout_div = document.getElementById("myntra_logout");
-        var logout = `<button class="logout_style" onclick="myntra_logout()"><i class="fa-solid fa-arrow-right-from-bracket"></i> <h5>logout</h5></button>`;
-        logout_div.innerHTML = logout;
-
-    } else {
-        console.log("Current User is absent");
-    }
 
     var ls = JSON.parse(localStorage.getItem("myntra_prodData"));
     var myntra_data = "";
